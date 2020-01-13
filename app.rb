fc3566a45c4c49ede391d6d2cf6e6738a07d17a0
Saul Ordonez <sauleordonez@gmail.com>
2020-01-12 17:31:39 -0800

    add routes in app.rb and content for all erb view files

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
+require('./lib/definition')
 require('pry')
 also_reload('lib/**/*.rb')
 
@@ -9,23 +10,22 @@ get('/') do
   erb(:words)
 end
 
-# get('/words') do
-#   if params["search"]
-#     @words = Word.search_by_word(params[:search])
-#   else
-#     @words = Word.all
-#   end
-#   erb(:words)
-# end
+get ('/words') do
+  if params["search"]
+    @words = Word.search(params[:search])
+  else
+    @words = Word.all
+  end
+  erb(:words)
+end
 
 get('/words/new') do
   erb(:new_word)
 end
 
 post('/words') do
-  word = params[:word]
-  definition = params[:definition]
-  word = Word.new(word, nil, definition)
+  name = params[:name]
+  word = Word.new(name, nil)
   word.save()
   @words = Word.all()
   erb(:words)
@@ -43,7 +43,7 @@ end
 
 patch('/words/:id') do
     @word = Word.find(params[:id].to_i())
-    @word.update(params[:name], params[:definition])
+    @word.update(params[:word], params[:image])
   @words = Word.all
   erb(:words)
 end
@@ -53,4 +53,30 @@ delete('/words/:id') do
   @word.delete()
   @words = Word.all
   erb(:words)
+end
+
+get('/words/:id/definitions/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+end
+
+post('/words/:id/definitions') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new(params[:definition_text], @word.id, nil)
+  definition.save()
+  erb(:word)
+end
+
+patch('/words/:id/definitions/:definition_id') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.update(params[:text], @word.id)
+  erb(:word)
+end
+
+delete('/words/:id/definitions/:definition_id') do
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.delete
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
 end
