0d4a51b28263e3f0cec65eab52e0494ea5341925
Saul Ordonez <sauleordonez@gmail.com>
2020-01-19 16:09:06 -0800

    add integration specs and successfully pass them

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -5,60 +5,35 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('create a word path', {:type => :feature}) do
-  it('creates an word and then goes to the word definition page') do
-    word = Word.new("Hello", nil)
-    word.save
-    visit("/")
-    click_on("Add a new word")
-    fill_in("wordname", :with => "Hello")
-    click_on("Go!")
+  it('creates a word and then goes to the word page') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'Hello')
+    click_on('Go!')
     expect(page).to have_content('Hello')
-
   end
 end
-#
-# describe('create a word path', {:type => :feature}) do
-#   it('creates an word and then goes to the word definition page') do
-#     word = Word.new("Hi", nil)
-#     word.save
-#     visit("/words/#{word.id}")
-#     fill_in('definition_name', :with => 'How are you?')
-#     click_on('Add definition')
-#     expect(page).to have_content('How are you?')
-#   end
-# end
 
+describe('edit a word', {:type => :feature}) do
+  it('creates a word and then edits it') do
+    word = Word.new("Hello", nil)
+    word.save
+    visit("/words/#{word.id}/edit")
+    fill_in('word_name', :with => 'Goodbye')
+    click_on('Update')
+    expect(page).to have_content('Goodbye')
+  end
+end
 
-
-    # describe('create a word path', {:type => :feature}) do
-    #   it('creates an word and then goes to the word definition page') do
-    #     word = Word.new("Hello", nil)
-    #     word.save
-    #     visit("/words/#{word.id}/definitions/new")
-    #     fill_in('definition', :with => 'How are you?')
-    #     click_on('Go!')
-    #     expect(page).to have_content('How are you?')
-    #   end
-    # end
-    #
-    # describe('edit a word', {:type => :feature}) do
-    #   it('creates an word and then edits it') do
-    #     word = Word.new("Hello", nil)
-    #     word.save
-    #     visit("/words/#{word.id}/edit")
-    #     fill_in('name', :with => 'Goodbye')
-    #     click_on('Update')
-    #     expect(page).to have_content('Goodbye')
-    #   end
-    # end
-    #
-    # describe('create a definition', {:type => :feature}) do
-    #   it('creates a definition for a word') do
-    #     word = Word.new("Hello", nil)
-    #     word.save
-    #     visit("/words/#{word.id}/definitions/new")
-    #     fill_in('definition', :with => 'How are you?')
-    #     click_on('Go!')
-    #     expect(page).to have_content('How are you?')
-    #   end
-    # end
+describe('create a definition', {:type => :feature}) do
+  it('creates a definition for a word') do
+    word = Word.new("Hello", nil)
+    word.save
+    definition = Definition.new('How are you?', nil, nil)
+    definition.save
+    visit("/words/#{word.id}")
+    fill_in('definition_content', :with => 'How are you?')
+    click_on('Add definition')
+    expect(page).to have_content('How are you?')
+  end
+end
